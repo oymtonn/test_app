ccbe60c881588e972b39303dc37f2417ebafcd3f
Myo Aung <myoaungg213@gmail.com>
2026-03-31 17:31:34 -0700

    onboarding route

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,19 +1,14 @@
 import "@/global.css";
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function App() {
+const Onboarding = () => {
   return (
     <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
-      <Link
-        href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Go to Onboarding
-      </Link>
     </View>
   );
-}
+};
+
+export default Onboarding;
